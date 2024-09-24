c3971e2dc86abcb41d7cbe68414d1af7b38dfda9
yinchaogao <yinchaogao@deeproute.ai>
2024-09-24 11:52:28 +0800

    update

--- degine/proposal/demo.cc
+++ degine/proposal/demo.cc
@@ -7,26 +7,7 @@
 #include "degine/ir/onnx.pb.h"
 #include "degine/proposal/InferSession.h"
 #include "degine/proposal/KernelRegistry.h"
-
-template <typename Inhert> class GeneralOpKernel : public OpKernel {
-public:
-  GeneralOpKernel(InferSession &sess, const OpInfo &op_info)
-      : OpKernel(sess, op_info) {
-    for (auto i = 0ul, e = op_info.InputCount(); i < e; i++) {
-      islots_.emplace_back(sess.GetOperand(op_info.Input(i)));
-    }
-    for (auto i = 0ul, e = op_info.OutputCount(); i < e; i++) {
-      oslots_.emplace_back(sess.GetOperand(op_info.Output(i)));
-    }
-  }
-
-  OperandInfo *Input(int idx) { return islots_[idx]; }
-  OperandInfo *Output(int idx) { return oslots_[idx]; }
-
-private:
-  std::vector<OperandInfo *> islots_;
-  std::vector<OperandInfo *> oslots_;
-};
+#include "degine/proposal/kernel.h"
 
 struct AddKernel : public GeneralOpKernel<AddKernel> {
   using GeneralOpKernel::GeneralOpKernel;
