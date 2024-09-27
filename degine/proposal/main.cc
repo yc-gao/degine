46a37b885e959ef7b1b08cb32bf20fe61fd3020b
yinchaogao <yinchaogao@deeproute.ai>
2024-09-27 17:25:32 +0800

    update

--- degine/proposal/main.cc
+++ degine/proposal/main.cc
@@ -1,6 +1,8 @@
 #include <cassert>
 #include <fstream>
 
+#include "fmt/base.h"
+
 #include "degine/cpu/CpuInferSession.h"
 #include "degine/ir/GraphModule.h"
 #include "degine/ir/onnx.pb.h"
@@ -13,7 +15,19 @@ int main(int argc, char *argv[]) {
 
   GraphModule g(model_pb);
   CpuInferSession sess(g);
+
+  std::vector<float> x(28 * 28, 1);
+
+  sess.SetBuffer("x", x.data());
+  sess.SetBuffer("y", x.data());
   sess.Infer();
+
+  for (auto i = 0ul; i < x.size();) {
+    for (auto j = 0ul; j < 16 && i < x.size(); j++, i++) {
+      fmt::print("{} ", x[i]);
+    }
+    fmt::print("\n");
+  }
 
   return 0;
 }
