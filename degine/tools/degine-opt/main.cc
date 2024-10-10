ad1765f7a4779729586a5f8032133bef1c997683
yinchaogao <yinchaogao@deeproute.ai>
2024-10-10 10:21:42 +0800

    update

--- degine/tools/degine-opt/main.cc
+++ degine/tools/degine-opt/main.cc
@@ -3,13 +3,24 @@
 #include "mlir/InitAllExtensions.h"
 #include "mlir/InitAllPasses.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
+#include "stablehlo/conversions/linalg/transforms/Passes.h"
+#include "stablehlo/conversions/tosa/transforms/Passes.h"
+#include "stablehlo/dialect/Register.h"
+#include "stablehlo/reference/InterpreterPasses.h"
+#include "stablehlo/transforms/Passes.h"
 
 int main(int argc, char **argv) {
   mlir::registerAllPasses();
+  mlir::stablehlo::registerPassPipelines();
+  mlir::stablehlo::registerPasses();
+  mlir::stablehlo::registerStablehloLinalgTransformsPasses();
+  mlir::stablehlo::registerInterpreterTransformsPasses();
+  mlir::tosa::registerStablehloTOSATransformsPasses();
 
   mlir::DialectRegistry registry;
   mlir::registerAllDialects(registry);
   mlir::registerAllExtensions(registry);
+  mlir::stablehlo::registerAllDialects(registry);
 
   return mlir::asMainReturnCode(mlir::MlirOptMain(
       argc, argv, "degine modular optimizer driver\n", registry));
